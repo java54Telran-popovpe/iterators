72abb2154a55383cccb1ee9dccf98feb88bc5c6d
Petr Popov <popov.pe@gmail.com>
2024-04-04 20:06:00 +0300

    CW #13 initial code

---
+++ src/telran/numbers/test/RangeTest.java
@@ -0,0 +1,26 @@
+package telran.numbers.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.numbers.Range;
+
+class RangeTest {
+
+	@Test
+	void iterableIteratorTest() {
+		Integer[] expected = { 5,6,7,8,9,10};
+		Range range = Range.getRange(5, 10);
+		assertArrayEquals( expected, toArrayFromIterable(new Integer[expected.length], range));
+	}
+	
+	private <T> T[] toArrayFromIterable( T[] array, Iterable<T> iterable) {
+		int index = 0;
+		for ( T obj: iterable ) {
+			array[index++] = obj;
+		}
+		return array;
+	}
+
+}
